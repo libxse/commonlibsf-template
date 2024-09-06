e4cdd86549d3e48f94903199c500007e398bb3dc
Qudix <17361645+Qudix@users.noreply.github.com>
2024-09-05 23:16:35 -0500

    feat: init

---
+++ xmake.lua
@@ -0,0 +1,41 @@
+-- set minimum xmake version
+set_xmakever("2.8.2")
+
+-- includes
+includes("lib/commonlibsf")
+
+-- set project
+set_project("commonlibsf-template")
+set_version("0.0.0")
+set_license("GPL-3.0")
+
+-- set defaults
+set_languages("c++23")
+set_warnings("allextra", "error")
+set_defaultmode("releasedbg")
+
+-- set policies
+set_policy("package.requires_lock", true)
+
+-- add rules
+add_rules("mode.releasedbg", "mode.debug")
+add_rules("plugin.vsxmake.autoupdate")
+
+-- setup targets
+target("commonlibsf-template")
+    -- add dependencies to target
+    add_deps("commonlibsf")
+
+    -- add commonlibsf plugin
+    add_rules("commonlibsf.plugin", {
+        name = "commonlibsf-template",
+        author = "Author Name",
+        description = "Plugin Description",
+        email = "user@site.com"
+    })
+
+    -- add source files
+    add_files("src/*.cpp")
+    add_headerfiles("src/*.h")
+    add_includedirs("src")
+    set_pcxxheader("src/pch.h")
