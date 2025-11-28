2ca6b6695b9ed6f145e89a85caf9b517c057a7be
Craig Robinson <craig.robinson87@ntlworld.com>
2025-11-28 14:00:49 +0000

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include "SFSE/API.h"
 
-SFSE_PLUGIN_PRELOAD(const SFSE::PreLoadInterface* a_sfse)
-{
-	SFSE::Init(a_sfse);
+#include <spdlog/spdlog.h>
 
-	return true;
+extern "C"
+{
+	bool SFSEPLugin_Load(const SFSE::LoadInterface* sfse)
+	{
+		SFSE::Init(sfse);
+
+		return true;
+	}
 }
 
-SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse)
-{
-	SFSE::Init(a_sfse);
 
-	REX::INFO("Hello World!");
-
-	return true;
-}
