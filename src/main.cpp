a525ea8498aeaa113e7d0a8c2502c84cb68ba2fc
Qudix <17361645+Qudix@users.noreply.github.com>
2026-08-23 12:01:07 -0500

    feat: update lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@ SFSE_PLUGIN_PRELOAD(const SFSE::PreLoadInterface* a_sfse)
 {
 	SFSE::Init(a_sfse);
 
+	REX::INFO("Hello PreLoaded World!");
+
 	return true;
 }
 
@@ -9,7 +11,7 @@ SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse)
 {
 	SFSE::Init(a_sfse);
 
-	REX::INFO("Hello World!");
+	REX::INFO("Hello Loaded World!");
 
 	return true;
 }
